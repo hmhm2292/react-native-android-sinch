594df9b63008daec0ba3003f373b65b65f3557a6
hmhm2292 <hyungmok.kim92@gmail.com>
2024-06-11 16:26:16 +0900

    feat(main): fix import calllog

--- android/src/main/java/com/androidsinch/AndroidSinchModule.kt
+++ android/src/main/java/com/androidsinch/AndroidSinchModule.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
+import android.provider.CallLog
 import android.provider.Settings
 import android.util.SparseArray
 import androidx.core.app.ActivityCompat
